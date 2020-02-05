75043ee6fb58deee09695f82ac314ffa99c61e30
Masatoshi Itoh <masatoshi9953@gmail.com>
2020-02-05 18:54:40 +0900

    add comments

--- src/helloworld.erl
+++ src/helloworld.erl
@@ -3,7 +3,10 @@
 -export([run_sample/0]).
 
 run_sample() ->
+    %% doubler関数をプロセスとして起動する
     Pid = spawn(fun() -> doubler() end),
+
+    %% doublerが受け取れる書式で 12 を送付
     Pid ! {calc, self(), 12},
     receive
         R1 ->
@@ -12,6 +15,7 @@ run_sample() ->
             io:format ("first receiver timed out~n", [])
     end,
 
+    %% doublerが受け取れない書式のメッセージ
     Pid ! {unknown, self(), "GEEEEE!"},
     receive
         R2 ->
@@ -20,6 +24,7 @@ run_sample() ->
             io:format ("second receiver timed out~n", [])
     end,
 
+    %% doublerに終了指示
     Pid ! stop,
     receive
         R3 ->
@@ -29,15 +34,20 @@ run_sample() ->
     end,
     0.
 
+%% メッセージで送られてきた値を2倍にして返す
+%% 実行し続けるために末尾再帰している
 doubler() ->
     receive
         {calc, Pid, Number} ->
+            %% メッセージ
             Pid ! Number * 2,
             doubler();
         stop ->
+            %% 停止メッセージが来たら、ログを出力して終了
             error_logger:error_report({self(), "stop requested. bye."}),
             0;
-        Unknown -> 
+        Unknown ->
+            %% それ以外のメッセージが来たらエラー報告
             error_logger:error_report({self(), unknown_message, Unknown}),
             doubler()
     end.
--- src/mpsample.erl
+++ src/mpsample.erl
@@ -2,26 +2,31 @@
 
 -export([run1/0, run2/0]).
 
+%% run1は、リスト操作でリストの値を2倍にしている
 run1() ->
     lists:map(fun(A) -> A * 2 end, [1,2,3,4,5,6,7,8,9,10]).
 
+%% run2は、リストの要素1つ1つにプロセスを起動している
 run2() ->
     RequesterPid = self(),
+    %% プロセス生成
     lists:map(
         fun(A) -> spawn(fun() -> doubler(RequesterPid, A) end) end,
         [1,2,3,4,5,6,7,8,9,10]),
+    %% 受信処理
     receiver2([]).
 
+%% タイムアウトするまで受信してリストに格納し続ける
 receiver2(Acc) ->
     receive
         {_A, Result} -> receiver2([Result | Acc])
     after 1000 -> Acc
     end.
 
-
+%% 指定のプロセスに計算結果をメッセージ送信する（数値のときだけ）
 doubler(RequesterPid, Number) when is_number(Number) ->
     RequesterPid ! {Number, Number * 2};
 
+%% 上記の条件（数値のときだけ）に合致しないときに実行されるdoubler関数
 doubler(RequesterPid, Payload) ->
     error_logger:error_report({error, doubler, not_number, RequesterPid, Payload, self()}).
-
